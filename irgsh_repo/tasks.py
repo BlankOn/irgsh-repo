ae69f6a340397428e0c4df520b9648ff0af419de
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-16 21:39:56 +0100

    Update task parameter

--- irgsh_repo/tasks.py
+++ irgsh_repo/tasks.py
@@ -15,14 +15,20 @@ class RebuildRepo(Task):
     exchange = 'repo'
     ignore_result = True
 
-    def run(self, spec_id, package, distribution, component,
+    def run(self, spec_id, package, version,
+            distribution, component, task_arch_list):
             arch_changes_list):
 
         try:
             # Rebuild repo for each architecture
-            for index, arch_changes in enumerate(arch_changes_list):
-                arch, changes = arch_changes
-                self.rebuild_repo(spec_id, arch, changes, component, distribution)
+            for index, task_arch in enumerate(task_arch_list):
+                task_id, arch = tash_arch
+                changes = '%(package)s_%(version)s_%(arch)s.changes' % \
+                          {'arch': arch,
+                           'package': package,
+                           'version': version}
+                changes_file = os.path.join(settings.INCOMING, arch, changes)
+                self.rebuild_repo(spec_id, arch, changes_file, component, distribution)
 
             # Report all done
             manager.update_status(spec_id, manager.COMPLETE)
@@ -30,14 +36,13 @@ class RebuildRepo(Task):
         except RepoRebuildError:
             pass
 
-    def rebuild_repo(self, spec_id, arch, changes, component, distribution):
+    def rebuild_repo(self, spec_id, arch, changes_file, component, distribution):
         try:
             # Logger
             fd, fname = tempfile.mkstemp()
             f = gzip.open(fname, 'wb')
 
             # Build repo
-            changes_file = os.path.join(settings.INCOMING, changes)
             cmd = 'reprepro -b %(base)s -C %(component)s include %(dist)s %(changes)s' % \
                   {'base': settings.REPO_DIR,
                    'component': component,
