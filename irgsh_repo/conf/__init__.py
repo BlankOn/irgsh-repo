4bb122688e9b0be6352f3bbb516b89c0a5b3add2
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-16 20:24:54 +0100

    Add settings

---
+++ irgsh_repo/conf/__init__.py
@@ -0,0 +1,158 @@
+"""
+Settings and configuration for irgsh-repo.
+
+This module is heavily influenced by and has some codes borrowed from Django.
+Licensed under BSD License.
+"""
+
+import os
+from glob import glob
+from ConfigParser import SafeConfigParser, NoOptionError
+
+from importlib import import_module
+
+from irgsh_repo.conf import global_settings
+
+ENVIRONMENT_VARIABLE = 'IRGSH_REPO_CONFIG'
+
+CONFIG_MAPPING = {
+    'irgsh': {
+        'result-dir': 'RESULT_DIR',
+        'server': 'SERVER',
+        'incoming': 'INCOMING',
+        'ssl-cert': 'SSL_CERT',
+        'ssl-key': 'SSL_KEY',
+        'archs': 'ARCHITECTURES',
+        'workers': 'CELERYD_CONCURRENCY',
+        'repo-dir': 'REPO_DIR',
+    },
+    'queue': {
+        'host': 'BROKER_HOST',
+        'port': 'BROKER_PORT',
+        'username': 'BROKER_USER',
+        'password': 'BROKER_PASSWORD',
+        'vhost': 'BROKER_VHOST'
+    },
+}
+CONFIG_REQUIRED = {
+    'irgsh': ['server', 'archs', 'incoming', 'repo_dir'],
+    'queue': ['host', 'vhost'],
+}
+CONFIG_TYPE_MAPPER = {
+    'BROKER_PORT': int,
+    'ARCHITECTURES': lambda x: x.split()
+}
+
+def init_settings(settings):
+    # Define celery queues
+    queue = 'repo'
+    settings.CELERY_QUEUES = {
+        queue: {
+            'exchange': 'repo',
+            'exchange_type': 'topic',
+            'binding_key': 'repo',
+        }
+    }
+    settings.CELERY_DEFAULT_QUEUE = queue
+    settings.CELERY_DEFAULT_EXCHANGE = 'repo'
+    settings.CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
+    settings.CELERY_DEFAULT_ROUTING_KEY = 'repo'
+
+    imports = getattr(settings, 'CELERY_IMPORTS', ())
+    settings.CELERY_IMPORTS = ('irgsh_repo.tasks',) + imports
+
+def load_config(config_files):
+    # Load config
+    cp = SafeConfigParser()
+    found = False
+    for config_file in config_files:
+        try:
+            if os.path.exists(config_file):
+                cp.read(config_file)
+                found = True
+        except TypeError:
+            pass
+
+    if not found:
+        raise ValueError
+
+    # Load config values
+    config = {}
+    for section in CONFIG_MAPPING:
+        for key, name in CONFIG_MAPPING[section].items():
+            try:
+                value = cp.get(section, key)
+                config[name] = value
+            except NoOptionError:
+                if key in CONFIG_REQUIRED[section]:
+                    raise ValueError, 'Key not found: %s' % key
+
+    # Type mapping
+    for key, mapper in CONFIG_TYPE_MAPPER.items():
+        if key in config:
+            config[key] = mapper(config[key])
+
+    return config
+
+class Settings(object):
+    def __init__(self, config_files):
+        # Load default settings
+        for setting in dir(global_settings):
+            if setting == setting.upper():
+                setattr(self, setting, getattr(global_settings, setting))
+
+        # Load configuration_file
+        try:
+            config = load_config(config_files)
+        except ValueError, e:
+            raise ValueError, "Unable to read configuration files"
+
+        for key, value in config.items():
+            setattr(self, key, value)
+
+        # Fill up dynamic settings
+        init_settings(self)
+
+class LazySettings(object):
+    '''Lazy settings loader.
+
+    Load settings when it is first used.
+    '''
+    def __init__(self):
+        self._configured = False
+        self._settings = None
+
+    def _configure(self):
+        config_files = []
+
+        config_files = ['irgsh-repo.conf',
+                        '/etc/irgsh/repo/irgsh-repo.conf']
+        config_files.append(sorted(
+                        glob('/etc/irgsh/repo/irgsh-repo.conf.d/*.conf')))
+
+        try:
+            config_file = os.environ[ENVIRONMENT_VARIABLE]
+            config_files.append(config_file)
+        except KeyError:
+            pass
+
+        self._settings = Settings(config_files)
+        self._configured = True
+
+    def __getattr__(self, name):
+        if not self._configured:
+            self._configure()
+        return getattr(self._settings, name)
+
+    def __hasattr__(self, name):
+        if not self._configured:
+            self._configure()
+        return hasattr(self._settings, name)
+
+    def __dir__(self):
+        if not self._configured:
+            self._configure()
+        return dir(self._settings)
+
+settings = LazySettings()
+
---
+++ irgsh_repo/conf/global_settings.py
@@ -0,0 +1,10 @@
+CELERYD_CONCURRENCY = 1
+
+CELERY_RESULT_BACKEND = "database"
+CELERY_RESULT_DBURI = "sqlite:///irgsh-repo-celery.db"
+CELERY_TRACK_STARTED = True
+
+LOCAL_DATABASE = 'sqlite:///irgsh-repo.db'
+SSL_CERT = None
+SSL_KEY = None
+
