7923f29338f6dc85c4c71f45b9b075c0e1f2c8a6
Fajran Iman Rusadi <fajran@gmail.com>
2011-01-27 15:42:10 +0100

    Add custom SCP handler

--- irgsh_repo/manager.py
+++ irgsh_repo/manager.py
@@ -10,6 +10,8 @@ COMPLETE = 1
 
 URL_UPDATE_STATUS = '%(host)s/build/%(spec_id)s/repo/status/'
 URL_PING = '%(host)s/worker/+ping/'
+URL_SPEC_INFO = '%(host)s/build/%(spec_id)s/info/'
+URL_TASK_INFO = '%(host)s/task/%(task_id)s/info/'
 
 def update_status(spec_id, status, arch=None):
     host = settings.SERVER.rstrip('/')
@@ -26,4 +28,16 @@ def ping():
 
     param = {}
     send_message(url, param)
+
+def get_spec_info(spec_id):
+    host = settings.SERVER.rstrip('/')
+    url = URL_SPEC_INFO % {'host': host, 'spec_id': spec_id}
+
+    return json.loads(send_message(url))
+
+def get_task_info(task_id):
+    host = settings.SERVER.rstrip('/')
+    url = URL_TASK_INFO % {'host': host, 'task_id': task_id}
+
+    return json.loads(send_message(url))
 
---
+++ irgsh_repo/serve.py
@@ -0,0 +1,107 @@
+import re
+import sys
+import os
+import getopt
+from subprocess import Popen
+
+from irgsh_repo import manager
+from irgsh_repo.conf import settings
+
+class ScpServe(object):
+    def __init__(self, argv, cmd):
+        self.argv = argv
+        self.cmd = cmd
+
+    def start(self):
+        try:
+            self._verify_cmd()
+
+            newcmd = self._start()
+            assert newcmd is not None, 'Unable to rebuild command'
+
+            self._exec(newcmd)
+        except Exception, e:
+            print >>sys.stderr, 'Error: %s' % e
+            return False
+        return True
+
+    def _verify_cmd(self):
+        p = self.cmd.split()
+        assert p[0] == 'scp', 'Not SCP'
+
+    def _exec(self, cmd):
+        p = Popen(cmd, shell=True)
+        p.communicate()
+
+        assert p.returncode == 0, 'Return code not zero'
+
+    def _start(self):
+        assert len(self.argv) == 3, 'Missing argument'
+        client_type, identifier = self.argv[1:]
+
+        assert client_type in ['builder', 'taskinit'], 'Unknown client type'
+        if client_type == 'builder':
+            return self._handle_builder(identifier)
+
+        elif client_type == 'taskinit':
+            return self._handle_taskinit(identifier)
+
+        return None
+
+    def _handle_builder(self, name):
+        spec_id, task_id = self._parse_task_id(self._get_target())
+        info = manager.get_task_info(task_id)
+
+        assert info['builder'] == name, 'Invalid builder'
+        assert info['spec_id'] == spec_id, 'Invalid target'
+
+        path = os.path.join(settings.INCOMING, str(spec_id), task_id)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        return 'scp -d -t %s' % path
+
+    def _handle_taskinit(self, worker_id):
+        spec_id = self._parse_spec_id(self._get_target())
+        info = manager.get_spec_info(spec_id)
+
+        path = os.path.join(settings.INCOMING, str(spec_id), 'source')
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        return 'scp -d -t %s' % path
+
+    def _get_target(self):
+        args = self.cmd.split()[1:]
+        optlist, args = getopt.getopt(args, 'dtf')
+
+        assert len(args) == 1, 'Unable to get target'
+
+        return args[0]
+
+    def _parse_task_id(self, target):
+        p = re.compile(r'^incoming/(\d+)/(\d+\.\d+\.\d+)/?$')
+        m = p.match(target)
+        if m:
+            return int(m.group(1)), m.group(2)
+        raise ValueError, 'Invalid target'
+
+    def _parse_spec_id(self, target):
+        p = re.compile(r'^incoming/(\d+)/source/?$')
+        m = p.match(target)
+        if m:
+            return int(m.group(1))
+        raise ValueError, 'Invalid target'
+
+def main():
+    cmd = os.environ.get('SSH_ORIGINAL_COMMAND', None)
+    if cmd is None:
+        print >>sys.stderr, 'Error: Missing value: SSH_ORIGINAL_COMMAND'
+        sys.exit(1)
+
+    os.chdir(os.path.expanduser('~'))
+
+    serve = ScpServe(sys.argv, cmd)
+    if not serve.start():
+        sys.exit(1)
+
