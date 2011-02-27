cc102cf2181f6a8c3bfa91989a956b501fa7d2db
Fajran Iman Rusadi <fajran@gmail.com>
2011-02-27 12:01:04 +0100

    Add python-debian to the dependency list

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name='irgsh-repo',
       entry_points={'console_scripts': ['irgsh-repo = irgsh_repo.main:main',
                                         'irgsh-upload-serve = irgsh_repo.serve:main',
                                         'irgsh-update-keys = irgsh_repo.update_keys:main']},
-      install_requires=['setuptools', 'celery<2.2', 'amqplib', 'poster', 'sqlalchemy'],
+      install_requires=['setuptools', 'celery<2.2', 'amqplib', 'poster',
+                        'sqlalchemy', 'python-debian'],
      )
 
