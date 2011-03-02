144f22720d51bc3a3424469806acae60e717b1f6
Fajran Iman Rusadi <fajran@gmail.com>
2011-03-02 22:25:10 +0100

    Wrap settings for celery with DictAttribute

--- irgsh_repo/loader.py
+++ irgsh_repo/loader.py
@@ -1,10 +1,11 @@
 from celery.loaders.base import BaseLoader
+from celery.datastructures import DictAttribute
 
 class IrgshRepoLoader(BaseLoader):
     def read_configuration(self):
         from irgsh_repo.conf import settings
         self.configured = True
-        return settings
+        return DictAttribute(settings)
 
     def on_worker_init(self):
         self.import_default_modules()
