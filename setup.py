ec5bf7586e897988dd4da0b78614914ae42a3619
Fajran Iman Rusadi <fajran@gmail.com>
2011-05-11 06:27:39 +0200

    Use simplejson by default

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name='irgsh-repo',
       entry_points={'console_scripts': ['irgsh-repo = irgsh_repo.main:main',
                                         'irgsh-upload-serve = irgsh_repo.serve:main',
                                         'irgsh-update-keys = irgsh_repo.update_keys:main']},
-      install_requires=['setuptools', 'celery>=2.2', 'poster', 'python-debian'],
+      install_requires=['setuptools', 'celery>=2.2', 'poster', 'python-debian',
+                        'simplejson'],
      )
 
